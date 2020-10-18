2f70778de4af64498fddf4b818479f8d5a75291f
jod35 <jod35@test.com>
2020-10-18 13:30:10 +0300

    successful user creation in remote postgresql DB

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 from flask import Flask, render_template
 from forms import RegistrationForm
+from models import User
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://pvgvneqzpomofd:ddea9a0a037cebcaaf16c2ab67ee5edad8c32cb66f1922e4c4c187dbe9809969@ec2-54-86-57-171.compute-1.amazonaws.com:5432/daen3mh5aip61s'
 
+db=SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        return "Create Success"
+        username=form.username.data
+        password=form.password.data
 
+        user_obj=User.query.filter_by(username=username).first()
+
+        if user_obj:
+            return "SomeOne has already taken this username"
+
+        new_user=User(username=username,password=password)
+
+        new_user.create()
+
+        return "Inserted Into DB!"
     context = {
         'form': form
     }
